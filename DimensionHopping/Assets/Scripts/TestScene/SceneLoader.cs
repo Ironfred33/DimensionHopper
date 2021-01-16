cba6a9e9fb0d060412a69ffa7afce4949f23d8b1
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-16 13:38:41 +0100

    - Zielpfosten hinzugefügt - Sceneloader hinzugefügt

---
+++ DimensionHopping/Assets/Scripts/TestScene/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    // Lädt die nächste Szene
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    
+    // Lädt erste Szene (vermutlich Hauptmenü dann später)
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
