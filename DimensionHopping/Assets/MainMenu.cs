23fb5b1a2b19e8b2b0de4d77125122e5bf9e6d20
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-04-29 12:49:35 +0200

    - added TextMeshPro - added simple Main Menu

---
+++ DimensionHopping/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneLoader sceneLoad;
+    public void StartGame()
+    {
+        sceneLoad.LoadNextScene();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
