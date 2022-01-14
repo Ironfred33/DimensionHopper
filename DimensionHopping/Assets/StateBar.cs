8a9c5e7e840a0f8dc0611ace91a2b2b6dee01892
Frederik Exner <frederik.exner@uni-bayreuth.de>
2022-01-14 09:38:42 +0100

    - State bar for 2D/3D switch - Lighting adjustments

---
+++ DimensionHopping/Assets/StateBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateBar : MonoBehaviour
+{
+    public CameraController camControl;
+    public Image square;
+    public Image cube;
+
+    public Color _activatedColor;
+    public Color _deactivedColor;
+
+    private void Start()
+    {
+        _activatedColor.a = 1f;
+        _deactivedColor.a = 0.5f;
+        
+    }
+    void Update()
+    {
+        if(camControl._is2DView)
+        {
+            square.color = _activatedColor;
+            cube.color = _deactivedColor;
+        }
+        else
+        {
+            square.color = _deactivedColor;
+            cube.color = _activatedColor;
+        }
+
+    }
+    
+}
