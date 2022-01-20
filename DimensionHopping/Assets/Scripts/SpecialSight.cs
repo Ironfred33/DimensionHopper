155b1c47c367a5a272fcafc091344f23bdec5fa1
Chris Reimelt <chris.reimelt@gmail.com>
2022-01-20 10:14:28 +0100

    - Zwischenstand SpecialSight Mechanik

--- DimensionHopping/Assets/Scripts/SpecialSight.cs
+++ DimensionHopping/Assets/Scripts/SpecialSight.cs
@@ -7,22 +7,36 @@ public class SpecialSight : MonoBehaviour
 
     public Camera camera;
 
+    public TransformPositionOnPerspective scriptPGO;
+
+    public Vector3 transformFirstPoint;
+    public Vector3 transformSecondPoint;
+
+    public GameObject copy;
+
+    public bool finishedCoolDown;
+
+    public bool finishedSightTime;
+
+
+    public float coolDown;
+    public float sightTime;
     public float range;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if(Input.GetKeyDown(KeyCode.V))
+        if (Input.GetKeyDown(KeyCode.V))
         {
             ShootRay();
         }
-        
+
     }
 
 
@@ -31,11 +45,44 @@ public class SpecialSight : MonoBehaviour
 
         RaycastHit hit;
 
-        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
+
+            if (hit.transform.tag == "PGOxPositive" || hit.transform.tag == "PGOzPositive" || hit.transform.tag == "PGOxNegative" || hit.transform.tag == "PGOzNegative")
+            {
+
+                Debug.Log(hit.transform.tag);
+
+                scriptPGO = hit.transform.GetComponent<TransformPositionOnPerspective>();
+
+                transformFirstPoint = scriptPGO.transformFirstPoint;
+                transformSecondPoint = scriptPGO.transformSecondPoint;
+
+                CreateCopy(hit);
+
+            }
 
         }
 
     }
+
+    void CreateCopy(RaycastHit hit)
+    {
+
+        // gibt alle gameobjects zurück, auch den player, wenn er zu dem zeitpunkt auf der plattform steht
+        copy = hit.transform.gameObject;
+
+        if(hit.collider.gameObject.transform.position == transformFirstPoint) Instantiate(copy, transformSecondPoint, Quaternion.identity);
+        else if(hit.collider.gameObject.transform.position == transformSecondPoint) Instantiate(copy, transformFirstPoint, Quaternion.identity);
+        
+
+    }
+
+    IEnumerator WaitForSecond(float time)
+    {
+
+        return null;
+    }
+
+
 }
