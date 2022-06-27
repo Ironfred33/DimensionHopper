567af7d64121ad3175a781ca36c5261b1e958b6d
Chris <chris.reimelt@gmail.com>
2022-06-27 16:17:32 +0200

    - Squeeze Mechanic test

---
+++ DimensionHopping/Assets/Squeezable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squeezable : MonoBehaviour
+{
+
+    public float maximumSqueeze;
+
+    public float squeezeSpeed;
+    public float squeezeAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.transform.tag == "Squeezer")
+        {
+            Debug.Log("SQUEEEZE");
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+       if(other.transform.tag == "Squeezer")
+       {
+
+    
+       } 
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.transform.tag == "Squeezer")
+        {
+            Debug.Log("NON SQUEEZE");
+        }
+    }
+}
